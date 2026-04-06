4c4c9419407af216e99119c05261a7186b573f05
sethtobeck <satobeck1@gmail.com>
2026-04-06 14:07:26 -0700

    second test jp5s whatever

---
+++ self_portrait/self_portrait.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas(600, 600);
+background(177, 181, 176);
+strokeWeight(0);
+}
+
+
+function draw() {
+  
+ellipse(300, 250, 300, 300);
+strokeWeight(0); 
+rect(200, 300, 200, 200);
+strokeWeight(0);
+
+}
